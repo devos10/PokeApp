88a16d7c0ea3f9d8dd1cc4947ec23a9c1364c7d7
devos10 <111657011+devos10@users.noreply.github.com>
2024-11-25 00:58:47 -0600

    archivos

--- obtenerPokePropio.js
+++
@@ -1,9 +0,0 @@
-const obtenerPokePropio = ()=>{
-    const num = input.value;
-
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res)=>{
-        return res.data
-    }).then((res)=>{
-        console.log(res);
-    })
-}
